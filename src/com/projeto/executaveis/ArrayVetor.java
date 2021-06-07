b996871620a56fc3781df74ef397159aa2aed507
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-07 13:09:00 -0300

    Orientação Objetos - array - 07/06/2021

--- src/com/projeto/executaveis/ArrayVetor.java
+++ src/com/projeto/executaveis/ArrayVetor.java
@@ -1,18 +1,23 @@
 package com.projeto.executaveis;
 
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
     public static void main(String[] args) {
 
         //TIPO DE DADOS ARRAY - NOME DA VARIAVEL DO ARRAY - LIMITE DO ARRAY
-        float[] notas = new float[4];
-        notas[0] = 9.8f;
-        notas[1] = 5.5f;
-        notas[2] = 7.5f;
-        notas[3] = 7.5f;
+        String posicao = JOptionPane.showInputDialog("Quantas posições o array precisa ter: ");
 
+        float[] notas = new float[Integer.parseInt(posicao)];
 
-        System.out.println(notas);
+        for (int pos = 0; pos < notas.length; pos++) {
+            String valor = JOptionPane.showInputDialog("Coloque a nota: " + pos);
+            notas[pos] = Integer.parseInt(valor);
+        }
 
+        for (int counter = 0; counter < notas.length; counter++) {
+            System.out.println("nota: " + (counter + 1) + " é = "  + notas[counter]);
+        }
     }
 }
