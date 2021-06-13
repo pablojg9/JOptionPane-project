8f65f220d755e88a8d01b87ba8c3c2f33815595d
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-13 14:32:21 -0300

    Orientação Objetos - array - 13/06/2021

---
+++ src/com/projeto/executaveis/Matriz.java
@@ -0,0 +1,31 @@
+package com.projeto.executaveis;
+
+public class Matriz {
+    public static void main(String[] args) {
+
+        int[][] notas = new int[2][3];
+
+        notas[0][0] = 86;
+        notas[0][1] = 76;
+        notas[0][2] = 100;
+
+        notas[1][0] = 50;
+        notas[1][1] = 30;
+        notas[1][2] = 29;
+
+        for (int counter = 0; counter < notas.length; counter++) {
+            System.out.println("====================================");
+
+            for(int counter2 : notas[counter]){
+                System.out.println("Matriz: " + counter2);
+
+
+            }
+
+        }
+
+
+
+    }
+
+}
