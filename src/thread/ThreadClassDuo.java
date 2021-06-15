fe2c3a43b9a79f0611dd23ec8beacb97c740cc09
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-15 19:31:50 -0300

    Orientação Objetos - Thread - 15/06/2021

---
+++ src/thread/ThreadClassDuo.java
@@ -0,0 +1,16 @@
+package thread;
+
+public class ThreadClassDuo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for(int position = 0; position < 10; position++) {
+            Thread.sleep(10000);
+
+            System.out.println("Executando alguma rotina - envio de email");
+
+        }
+
+    }
+
+}
