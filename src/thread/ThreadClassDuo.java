0e2e93b4813d15bca2860385073fe775a2efc070
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-16 18:41:15 -0300

    Orientação Objetos - Thread - 16/06/2021

--- src/thread/ThreadClassDuo.java
+++ src/thread/ThreadClassDuo.java
@@ -1,15 +1,34 @@
 package thread;
 
+import javax.swing.JOptionPane;
+
 public class ThreadClassDuo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for(int position = 0; position < 10; position++) {
-            Thread.sleep(10000);
+        new Thread() {
+            @Override
+            public void run() {
+                for(int position = 0; position < 10; position++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-            System.out.println("Executando alguma rotina - envio de email");
+                    System.out.println("Executando alguma rotina - envio de email");
+                }
+                super.run();
+            }
+        }.start();
 
-        }
+
+        System.out.println("O código chegou no fim");
+
+        JOptionPane.showMessageDialog(null, "Sistema contino executando para o usuario");
+
+
+
 
     }
 
