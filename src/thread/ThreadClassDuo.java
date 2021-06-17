1383452be29b699a8f8bb14cde5cebc4ebe789b0
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-17 12:37:09 -0300

    Orientação Objetos - Thread 2 processamentos rodando em paralelo + vetor/array - 16/06/2021

--- src/thread/ThreadClassDuo.java
+++ src/thread/ThreadClassDuo.java
@@ -1,6 +1,7 @@
 package thread;
 
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class ThreadClassDuo {
 
@@ -9,7 +10,7 @@ public class ThreadClassDuo {
         new Thread() {
             @Override
             public void run() {
-                for(int position = 0; position < 10; position++) {
+                for (int position = 0; position < 10; position++) {
                     try {
                         Thread.sleep(2000);
                     } catch (InterruptedException e) {
@@ -22,35 +23,62 @@ public class ThreadClassDuo {
             }
         }.start();
 
+        /** Instanciando a CLASSE Thread e chamando métodos staticos dentro de uma nova Thread **/
+        Thread thread = new Thread(THREAD_VETOR);
+        Thread threadNotaFiscal = new Thread(THREAD_NOTA_FISCAL);
+
+        /** Startando as thread criadas **/
+        thread.start();
+        threadNotaFiscal.start();
+
         /**
          * Apredendo Thread rodando pro trás dos panos.
          *
          * THREAD em 2 processamento paralelos **/
-        new Thread() {
-            @Override
-            public void run() {
-                for(int position = 0; position < 10; position++) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    System.out.println("Executando alguma rotina - envio de nota fiscal");
-                }
-                super.run();
-            }
-        }.start();
-
-
-
-        System.out.println("O código chegou no fim");
-
-        JOptionPane.showMessageDialog(null, "Sistema contino executando para o usuario");
-
-
-
 
     }
 
+    /** EXECUTANDO A "NOTA FISCAL" EM UMA THREAD PARALELA **/
+    private static final Runnable THREAD_NOTA_FISCAL = new Runnable() {
+        @Override
+        /** SOBREPONDO O MÉTODO RUN() **/
+        public void run() {
+            for(int position = 0; position < 10; position++) {
+                /** TRATAMENTO DE RRO PARA USAR O MÉTODO STATIC SLEEP **/
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Executando alguma rotina - envio de nota fiscal");
+            }
+        }
+    };
+
+    /** Usando esse THREAD COM VETOR OU ARRAY **/
+    public static final Runnable THREAD_VETOR = new Runnable() {
+        @Override
+        public void run() {
+
+            float[] posicaoArray = new float[10];
+
+            for (int counter = 0; counter < posicaoArray.length; counter++) {
+                String inputDialog = JOptionPane.showInputDialog("Coloque o " + (counter + 1) + " primeiro número: ");
+
+
+                posicaoArray[counter] = Integer.parseInt(inputDialog);
+            }
+
+            /** Chamando a CLASS e instanciando o StringBuilder **/
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            /** Mostrando os número que o cliente colocou **/
+            for (float posicaoArrayLoop : posicaoArray) {
+                stringBuilder.append("número: " + posicaoArrayLoop + "\n");
+            }
+            JOptionPane.showMessageDialog(null, stringBuilder.toString());
+
+        }
+    };
 }
