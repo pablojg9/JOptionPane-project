3cb003d280fd8eb6e75c6c3d98206155c3fb04e2
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-18 18:18:13 -0300

    Orientação Objetos - Thread 2 processamentos rodando em paralelo + vetor/array - 18/06/2021

--- src/thread/ThreadClass.java
+++ src/thread/ThreadClass.java
@@ -3,15 +3,25 @@ package thread;
 public class ThreadClass {
     public static void main(String[] args) throws InterruptedException {
 
+        new Thread() {
+            @Override
+            public void run() {
+                for(int position = 0; position < 10; position++) {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("Executando alguma rotina - envio de email");
+
+                }
+                super.run();
+            }
+        }.start();
 
 
 
-        for(int position = 0; position < 10; position++) {
-            Thread.sleep(10000);
-
-            System.out.println("Executando alguma rotina - envio de email");
-
-        }
 
     }
 }
